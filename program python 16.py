8fedeaab6889aea1bc529adc3b2081ee0a6d22f8
JERUKA9 <jeruka9@gmail.com>
2019-01-24 12:36:59 +0100

    Add files via upload

---
+++ program python 16.py
@@ -0,0 +1,14 @@
+class Calculator():
+    
+    def add(self,a,b):
+        
+        result=a+b
+        print(result)
+        
+        def substract(self,a,b):
+            result=a-b
+            print(result)
+            
+            my_calc = Calculator()
+            Calc.add(10,5)
+            Calc.substract(10,5
