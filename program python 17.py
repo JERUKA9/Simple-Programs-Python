504f2a9f2b352f10cde52a7dd8de68ad5b5a09e4
JERUKA9 <jeruka9@gmail.com>
2019-01-26 15:19:38 +0100

    Add files via upload

---
+++ program python 17.py
@@ -0,0 +1,33 @@
+from unittest import result
+
+
+def __init__(self):
+
+    
+    def __del__(self):
+        
+        def __str__(self):
+        
+            
+            
+            
+            def add(self,a,b): 
+                
+                result = a+b
+                       
+        
+        def substract(self,a,b):
+            
+            result = a-b
+            
+            
+            result.add(10,5)
+            
+            result.substract(10,5)
+            
+            
+            
+        
+            
+            print(result)
+        
