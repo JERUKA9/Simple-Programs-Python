2567d2b03f519f070555b04c5ac8c39ebc7eac67
JERUKA9 <jeruka9@gmail.com>
2019-01-29 15:49:52 +0100

    Add files via upload

---
+++ program python 20.py
@@ -0,0 +1,12 @@
+def Celsius_to_Fahrenheit_Conventer(temperature):
+    
+    temperature = 273.15
+    
+    if temperature <  -273.15:
+        
+        print("too cold")
+        
+        
+        print(Celsius_to_Fahrenheit_Conventer(-300))
+        
+        
